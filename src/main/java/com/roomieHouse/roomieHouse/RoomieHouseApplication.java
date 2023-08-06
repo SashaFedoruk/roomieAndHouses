3e7c476a8e5a5ec74e5526678c1e9052b2399418
Sasha Fedoruk <sashafedoruk@MacBook-Pro.local>
2023-08-06 16:47:08 -0700

    Init project

---
+++ src/main/java/com/roomieHouse/roomieHouse/RoomieHouseApplication.java
@@ -0,0 +1,13 @@
+package com.roomieHouse.roomieHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomieHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomieHouseApplication.class, args);
+	}
+
+}
